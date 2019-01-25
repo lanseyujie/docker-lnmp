0959ecdeb9abfe73e5c2bf556496fdd7912117ae
Wildlife <admin@lanseyujie.com>
2019-01-25 11:36:32 +0800

    使用脚本初始化 data 目录

---
+++ initdir.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -xe
+
+# Clean
+if [ "${1#--}" = "rm" ]; then
+    rm -rf data/
+fi
+
+mkdir -p data/wwwlogs data/wwwdata data/wwwroot
+
+# wwwlogs
+mkdir -p data/wwwlogs/mysql data/wwwlogs/redis data/wwwlogs/php data/wwwlogs/nginx
+# Mysql
+touch data/wwwlogs/mysql/error.log
+# Redis
+touch data/wwwlogs/redis/redis.log
+# Php
+touch data/wwwlogs/php/access.log
+touch data/wwwlogs/php/error.log
+touch data/wwwlogs/php/slow.log
+# Nginx
+touch data/wwwlogs/nginx/access.log
+touch data/wwwlogs/nginx/error.log
+
+# wwwdata
+mkdir -p data/wwwdata/mysql data/wwwdata/redis
+
+# wwwroot
+mkdir -p data/wwwroot/default
+
+# test
+echo '<?php phpinfo();' > data/wwwroot/default/index.php
