698dd2e692defd563bddce465d3d1165ebdb4b93
Wildlife <admin@lanseyujie.com>
2019-02-12 11:03:29 +0800

    优化脚本

--- syncid.sh
+++ syncid.sh
@@ -5,8 +5,8 @@ UID=$(id -u $USER)
 GID=$(id -g $USER)
 
 if [ $UID -gt 0 ] && [ $GID -gt 0 ] ; then
-    docker-compose restart
+    docker-compose restart php
     docker exec php sh -c "apk --no-cache add shadow && usermod -u $UID www-data && groupmod -g $GID www-data && apk del shadow"
 fi
 
-docker-compose restart
+docker-compose restart php
