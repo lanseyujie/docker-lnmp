203676efeca4c7e64f3f2821751ae7b8d3594ec6
Wildlife <admin@lanseyujie.com>
2019-02-20 14:47:10 +0800

    Fix nginx 重启修改 id

--- syncid.sh
+++ syncid.sh
@@ -5,8 +5,11 @@ UID=$(id -u $USER)
 GID=$(id -g $USER)
 
 if [ $UID -gt 0 ] && [ $GID -gt 0 ] ; then
-    docker-compose restart php
     docker exec php sh -c "apk --no-cache add shadow && usermod -u $UID www-data && groupmod -g $GID www-data && apk del shadow"
+    
+    docker exec php sh -c "apk --no-cache add shadow && usermod -u $UID nginx && groupmod -g $GID nginx && apk del shadow"
 fi
 
 docker-compose restart php
+
+docker-compose restart nginx
