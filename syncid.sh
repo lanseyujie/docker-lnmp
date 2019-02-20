8a92954e5defef0cf3c6222ec41ddba36b184db2
Wildlife <admin@lanseyujie.com>
2019-02-20 14:55:21 +0800

    Fix 容器名称

--- syncid.sh
+++ syncid.sh
@@ -7,7 +7,7 @@ GID=$(id -g $USER)
 if [ $UID -gt 0 ] && [ $GID -gt 0 ] ; then
     docker exec php sh -c "apk --no-cache add shadow && usermod -u $UID www-data && groupmod -g $GID www-data && apk del shadow"
     
-    docker exec php sh -c "apk --no-cache add shadow && usermod -u $UID nginx && groupmod -g $GID nginx && apk del shadow"
+    docker exec nginx sh -c "apk --no-cache add shadow && usermod -u $UID nginx && groupmod -g $GID nginx && apk del shadow"
 fi
 
 docker-compose restart php
