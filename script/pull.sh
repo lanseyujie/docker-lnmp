3c5938d55ce3c84d35ceac8c7332f143790d12b6
Wildlife <admin@lanseyujie.com>
2019-12-11 14:39:49 +0800

    仓库仅存在amd64镜像

--- script/pull.sh
+++ script/pull.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -ex
 
+arch=$(dpkg --print-architecture)
+
+if [ $arch != "amd64" ]; then
+    echo "unsupported architecture: $arch"
+    exit;
+fi
+
 lnmp="mariadb redis php nginx"
 
 for i in $lnmp
