4fd3f35bec98e732f5997025f59448a4eb0127c2
Wildlife <admin@lanseyujie.com>
2021-12-10 18:02:07 +0800

    修正 docker 登录

--- script/build.sh
+++ script/build.sh
@@ -8,7 +8,7 @@ IMAGE="${REGISTRY_HOST}/${REGISTRY_NAMESPACE}/docker-lnmp-${NAME}:${VERSION}"
 cd "${NAME}"
 
 docker build --force-rm --compress --file Dockerfile --tag "${IMAGE}" .
-docker login "${REGISTRY_HOST}" -u "${REGISTRY_USERNAME}" -p "${REGISTRY_USERNAME}"
+echo "${REGISTRY_PASSWORD}" | docker login "${REGISTRY_HOST}" -u "${REGISTRY_USERNAME}" --password-stdin
 docker push "${IMAGE}"
 
 cd -
